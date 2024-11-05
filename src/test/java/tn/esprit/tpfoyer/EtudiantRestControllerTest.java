4b5c06b4c999340cda80f0f409f7497bcdd649c1
arij <m'hiri.arij@esprit.tn>
2024-11-05 12:02:11 +0100

    .

--- src/test/java/tn/esprit/tpfoyer/EtudiantRestControllerTest.java
+++ src/test/java/tn/esprit/tpfoyer/EtudiantRestControllerTest.java
@@ -46,95 +46,95 @@ public class EtudiantRestControllerTest {
 
     @Test
     public void testGetEtudiants() {
-        // Given
+
         List<Etudiant> etudiantList = Arrays.asList(etudiant);
         when(etudiantService.retrieveAllEtudiants()).thenReturn(etudiantList);
 
-        // When
+
         List<Etudiant> result = etudiantRestController.getEtudiants();
 
-        // Then
+
         assertNotNull(result);
         assertEquals(1, result.size());
         assertEquals(1L, result.get(0).getIdEtudiant());
 
-        // Verify that the service method was called once
+
         verify(etudiantService, times(1)).retrieveAllEtudiants();
     }
 
     @Test
     public void testRetrieveEtudiantParCin() {
-        // Given
+
         when(etudiantService.recupererEtudiantParCin(12345678L)).thenReturn(etudiant);
 
-        // When
+
         Etudiant result = etudiantRestController.retrieveEtudiantParCin(12345678L);
 
-        // Then
+
         assertNotNull(result);
         assertEquals(12345678L, result.getCinEtudiant());
 
-        // Verify that the service method was called once
+
         verify(etudiantService, times(1)).recupererEtudiantParCin(12345678L);
     }
 
     @Test
     public void testRetrieveEtudiantById() {
-        // Given
+
         when(etudiantService.retrieveEtudiant(1L)).thenReturn(etudiant);
 
-        // When
+
         Etudiant result = etudiantRestController.retrieveEtudiant(1L);
 
-        // Then
+
         assertNotNull(result);
         assertEquals(1L, result.getIdEtudiant());
 
-        // Verify that the service method was called once
+
         verify(etudiantService, times(1)).retrieveEtudiant(1L);
     }
 
     @Test
     public void testAddEtudiant() {
-        // Given
+
         when(etudiantService.addEtudiant(any(Etudiant.class))).thenReturn(etudiant);
 
-        // When
+
         Etudiant result = etudiantRestController.addEtudiant(etudiant);
 
-        // Then
+
         assertNotNull(result);
         assertEquals(1L, result.getIdEtudiant());
 
-        // Verify that the service method was called once
+
         verify(etudiantService, times(1)).addEtudiant(any(Etudiant.class));
     }
 
     @Test
     public void testRemoveEtudiant() {
-        // Do nothing when the service's remove method is called
+
         doNothing().when(etudiantService).removeEtudiant(1L);
 
-        // Call the controller method
+
         etudiantRestController.removeEtudiant(1L);
 
-        // Verify that the service method was called once
+
         verify(etudiantService, times(1)).removeEtudiant(1L);
     }
 
     @Test
     public void testModifyEtudiant() {
-        // Given
+
         when(etudiantService.modifyEtudiant(any(Etudiant.class))).thenReturn(etudiant);
 
-        // When
+
         Etudiant result = etudiantRestController.modifyEtudiant(etudiant);
 
-        // Then
+
         assertNotNull(result);
         assertEquals(1L, result.getIdEtudiant());
 
-        // Verify that the service method was called once
+
         verify(etudiantService, times(1)).modifyEtudiant(any(Etudiant.class));
     }
 }
